6ad40f596f24bf7f57e44197fa8c065018bedc61
Eric Lingerfelt <eric2@ucar.edu>
2023-01-12 12:43:04 -0700

    Removed unused imports. Applied PEP8.

--- jedi_obs_conventions_sprint/upgrade_aod_ioda_conventions.py
+++ jedi_obs_conventions_sprint/upgrade_aod_ioda_conventions.py
@@ -6,7 +6,6 @@ To run this script on VIIRS data users need to first run
 viirs_fix.py to fix the variable name
 """
 
-import sys
 import os
 import argparse
 from solo.configuration import Configuration
@@ -21,18 +20,18 @@ config = Configuration(input_file)
 dates = date_sequence(config.start, config.end, config.obs_frequency)
 
 for date in dates:
-  date = JediDate(date)
-  input_file = f'{config.source_path}/{date}/{config.provider}.obs.{config.obs_type}.{date}.{config.window_length}.nc4'
-  if not(os.path.exists(input_file)):
-    print( f'{input_file} does not exist')
-  else:
-    print( f'processing {input_file}')
+    date = JediDate(date)
+    input_file = f'{config.source_path}/{date}/{config.provider}.obs.{config.obs_type}.{date}.{config.window_length}.nc4'
+    if not (os.path.exists(input_file)):
+        print(f'{input_file} does not exist')
+    else:
+        print(f'processing {input_file}')
 
-  output_dir = f'{config.output_path}/{date}'
-  if not(os.path.exists(output_dir)):
-    print( f'{output_dir} does not exist')
-    os.makedirs(output_dir)
+    output_dir = f'{config.output_path}/{date}'
+    if not (os.path.exists(output_dir)):
+        print(f'{output_dir} does not exist')
+        os.makedirs(output_dir)
 
-  output_file = f'{output_dir}/{config.provider}.obs.{config.obs_type}.{date}.{config.window_length}.nc4'
+    output_file = f'{output_dir}/{config.provider}.obs.{config.obs_type}.{date}.{config.window_length}.nc4'
 
-  os.system(f'{config.jedi_build}/ioda-upgrade-v2-to-v3.x {input_file} {output_file} {config.obsspace_yaml}')
+    os.system(f'{config.jedi_build}/ioda-upgrade-v2-to-v3.x {input_file} {output_file} {config.obsspace_yaml}')
